6d8367faf2f6eff0b23a8b2ce76158584e35e348
romboooo <412902@niuitmo.ru>
2025-12-21 13:52:17 +0300

    generated types example

---
+++ generated/GeneratedTypes.fs
@@ -0,0 +1,36 @@
+﻿// AUTO-GENERATED FILE - DO NOT EDIT
+// Generated at: 2025-12-21 13:06:54
+// Generated from database schema
+
+namespace ORM.GeneratedTypes
+
+open System
+
+[<AttributeUsage(AttributeTargets.Property)>]
+type PrimaryKeyAttribute() =
+    inherit Attribute()
+
+type Orders = {
+    [<PrimaryKey>] id: int
+    userId: int option
+    productId: int option
+    quantity: int option
+    status: string
+    orderDate: System.DateTime option
+}
+
+type Products = {
+    [<PrimaryKey>] id: int
+    name: string
+    price: int
+    category: string
+    inStock: bool option
+}
+
+type Users = {
+    [<PrimaryKey>] id: int
+    username: string
+    email: string
+    age: int option
+    createdAt: System.DateTime option
+}
